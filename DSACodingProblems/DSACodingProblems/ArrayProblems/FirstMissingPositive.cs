985c3d471fb99422cf6be376a646bbff95d00ef4
Mohan Rathod <mohanrathod31@mail.com>
2024-06-01 23:58:19 +0530

    FirstMissingPositive and MissingNumbers

---
+++ DSACodingProblems/DSACodingProblems/ArrayProblems/FirstMissingPositive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSACodingProblems.ArrayProblems
+{
+    public class FirstMissingPositive
+    {
+        public int FindFirstMissingPositive(int[] nums) 
+        {
+            int missingNumber = 1;
+
+            Array.Sort(nums);
+
+            return missingNumber;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == missingNumber)
+                    missingNumber++;
+                else if (nums[i] > missingNumber)
+                    break;
+            }
+
+            return missingNumber;
+        }
+    }
+}
---
+++ DSACodingProblems/DSACodingProblems/ArrayProblems/MissingNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSACodingProblems.ArrayProblems
+{
+    public class MissingNumber
+    {
+        public int FindMissingNumber(int[] nums) 
+        {
+            int missingNumber = 0;
+            int sum = 0;
+            int actualSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            actualSum = (nums.Length * (nums.Length + 1)) / 2;
+            missingNumber = actualSum - sum;
+
+            return missingNumber;
+        
+        }
+    }
+}
